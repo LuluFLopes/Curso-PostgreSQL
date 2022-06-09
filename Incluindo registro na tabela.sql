4a1b462a0e632bb221f385a21639f2159a367f00
Luciano <luc.merce@hotmail.com>
2022-06-09 00:59:28 -0300

    Incluindo regustro na tabela

---
+++ Incluindo registro na tabela.sql
@@ -0,0 +1,23 @@
+-- Inserindo na Tabela --
+insert into aluno (
+ nome, 
+ cpf, 
+ observacao, 
+ idade,dinheiro,
+ altura,ativo,
+ data_nascimento,
+ hora_aula,
+ matriculado_em
+) 
+values(
+	'Diogo',
+	'12345678901',
+	'Texto Grande',
+	35,
+	100.50,
+	1.81,
+	true,
+	'1984-08-27',
+	'17:30:00',
+	'2020-02-08 12:32:45'
+);
